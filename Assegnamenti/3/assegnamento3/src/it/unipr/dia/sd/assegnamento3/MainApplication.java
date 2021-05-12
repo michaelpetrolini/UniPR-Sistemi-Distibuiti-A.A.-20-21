f05f394d4b5196aa552a12a95bf8250ca605263c
PetroliniM <michael.petrolini@omigrade.it>
2021-05-12 17:29:58 +0200

    embedded broker

--- Assegnamenti/3/assegnamento3/src/it/unipr/dia/sd/assegnamento3/MainApplication.java
+++ Assegnamenti/3/assegnamento3/src/it/unipr/dia/sd/assegnamento3/MainApplication.java
@@ -1,17 +1,21 @@
 package it.unipr.dia.sd.assegnamento3;
 
-import javax.jms.JMSException;
-
-import it.unipr.dia.sd.assegnamento3.statemachine.StateMachineException;
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
 
 public class MainApplication {
 
-	public static void main(String[] args) throws StateMachineException, JMSException {
+	public static void main(String[] args) throws Exception {
 		if (args.length > 0) {
 			String nodeType = args[0];
 			String id = args[1];
 			Integer nClients = Integer.valueOf(args[2]);
 			Integer nCoordinators = Integer.valueOf(args[3]);
+			
+			if (nodeType.equals("coordinator") && id.equals("1")) {
+				BrokerService broker = BrokerFactory.createBroker("broker:(tcp://localhost:61616)?persistent=false&useJmx=false");
+				broker.start();
+			}
 			
 			if (nodeType.equals("client")) {
 				System.out.println("Starting client n " + id);
